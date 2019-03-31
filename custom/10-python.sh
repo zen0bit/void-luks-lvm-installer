b92708e2c5ac3c82979fe0bb5584827b34f325ce
Ivan Mirić <imiric@gmail.com>
2019-03-31 19:56:22 +0200

    feat(custom): install additional Python packages

--- custom/10-python.sh
+++ custom/10-python.sh
@@ -4,9 +4,17 @@ xbps-install -Sy bzip2-devel libffi-devel liblzma-devel libressl-devel \
   ncurses-devel readline-devel sqlite-devel zlib-devel \
   || true
 
-# Install pyenv and specified Python version
+# Install pyenv, the specified Python version and additional packages
 su -w PYTHON_VERSION - ${USERACCT} <<'EOF'
-test -d ~/.pyenv || git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-PYENV_ROOT=$HOME/.pyenv PATH=$PYENV_ROOT/bin:$PATH pyenv install ${PYTHON_VERSION}
+export PYENV_ROOT=$HOME/.pyenv PATH=$PYENV_ROOT/bin:$PATH
+test -d $PYENV_ROOT || git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
+pyenv install ${PYTHON_VERSION}
 git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
+pyenv global ${PYHON_VERSION}
+
+# Python packages
+pip install glances pinboard
+
+# Fix bug in pinboard package
+chmod +x $PYENV_ROOT/versions/$PYTHON_VERSION/bin/pinboard
 EOF
