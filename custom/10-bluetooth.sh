86282a5cb488ec6a8ad695e590b58ef41f1231b8
Ivan Mirić <imiric@gmail.com>
2019-03-31 19:57:23 +0200

    fix(custom): enable Bluetooth services

--- custom/10-bluetooth.sh
+++ custom/10-bluetooth.sh
@@ -2,6 +2,10 @@
 
 xbps-install -Sy bluez obexfs || true
 
+# Enable services
+ln -sfn /etc/sv/dbus /var/service/
+ln -sfn /etc/sv/bluetoothd /var/service/
+
 if [ -n "$USERACCT" ]; then
   usermod -aG bluetooth ${USERACCT}
 fi
