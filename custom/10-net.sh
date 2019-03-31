47035968fe7001f416be4e8e92df0bc5f0a2f80a
Ivan Mirić <imiric@gmail.com>
2019-03-31 19:55:37 +0200

    fix(custom): install st as surf dependency to fix downloads

--- custom/10-net.sh
+++ custom/10-net.sh
@@ -8,5 +8,12 @@ ln -sfn /etc/sv/wpa_supplicant /etc/runit/runsvdir/default/
 
 # Download and compile Surf
 xbps-install -Sy gcr-devel glib-devel rofi webkit2gtk-devel || true
-test -d ~/Projects/surf || (git clone git@github.com:imiric/surf.git ~/Projects/surf)
+su -w SSH_AUTH_SOCK - ${USERACCT} <<'EOF'
+export GIT_SSH_COMMAND='ssh -o Hostname=172.16.1.5 -o Port=10022'
+test -d ~/Projects/surf || git clone git@git.imiric.lan:ivan/surf.git ~/Projects/surf
 (cd ~/Projects/surf && make install)
+
+# st is needed for surf's download functionality
+test -d ~/Projects/st || git clone git@git.imiric.lan:ivan/st.git ~/Projects/st
+(cd ~/Projects/st && make install)
+EOF
