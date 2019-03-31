6f6cb9f127f7bfd74fcba6774df575d51c941bed
Ivan Mirić <imiric@gmail.com>
2019-03-31 16:34:24 +0200

    feat(custom): add rsync package

--- custom/01-sys.sh
+++ custom/01-sys.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-xbps-install -Sy acpi base-devel dbus git htop lm_sensors readline strace xtools xz || true
+xbps-install -Sy \
+  acpi base-devel dbus git htop lm_sensors readline rsync strace xtools xz \
+  || true
