ef760b2347fa958c805768cba20318b7a478f08a
Ivan Mirić <imiric@gmail.com>
2019-03-31 19:58:15 +0200

    feat(custom): add bluez-alsa package for Bluetooth audio

--- custom/10-bluetooth.sh
+++ custom/10-bluetooth.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-xbps-install -Sy bluez obexfs || true
+xbps-install -Sy bluez bluez-alsa obexfs || true
 
 # Enable services
 ln -sfn /etc/sv/dbus /var/service/
 ln -sfn /etc/sv/bluetoothd /var/service/
+ln -sfn /etc/sv/bluez-alsa /var/service/
 
 if [ -n "$USERACCT" ]; then
   usermod -aG bluetooth ${USERACCT}
