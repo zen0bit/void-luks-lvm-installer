797b0356c2faca6c23534fcbaa7a11e3b5d0a07d
Ivan Mirić <imiric@gmail.com>
2019-04-02 19:04:59 +0200

    fix(custom): properly set SSH_AUTH_SOCK

--- custom/10-net.sh
+++ custom/10-net.sh
@@ -8,8 +8,9 @@ ln -sfn /etc/sv/wpa_supplicant /etc/runit/runsvdir/default/
 
 # Download and compile Surf
 xbps-install -Sy gcr-devel glib-devel rofi webkit2gtk-devel || true
-su -w SSH_AUTH_SOCK - ${USERACCT} <<'EOF'
-export GIT_SSH_COMMAND='ssh -o Hostname=172.16.1.5 -o Port=10022'
+su - ${USERACCT} <<'EOF'
+export GIT_SSH_COMMAND='ssh -o Hostname=172.16.1.5 -o Port=10022' \
+  SSH_AUTH_SOCK=/tmp/.gnupg/S.gpg-agent.ssh
 test -d ~/Projects/surf || git clone git@git.imiric.lan:ivan/surf.git ~/Projects/surf
 (cd ~/Projects/surf && make install)
 
