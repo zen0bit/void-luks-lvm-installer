335d202ba65b8bd821849f658b16513eec526f7a
Ivan Mirić <imiric@gmail.com>
2019-04-02 19:11:44 +0200

    fix(custom): add safety checks

--- custom/10-programming.sh
+++ custom/10-programming.sh
@@ -4,6 +4,6 @@ xbps-install -Sy emacs-gtk3 ripgrep vim || true
 
 su - ${USERACCT} <<'EOF'
 mkdir -p ~/src
-git clone https://github.com/so-fancy/diff-so-fancy.git ~/src/diff-so-fancy
-ln -s ~/src/diff-so-fancy/diff-so-fancy ~/.local/bin/
+test -d ~/src/diff-so-fancy || git clone https://github.com/so-fancy/diff-so-fancy.git ~/src/diff-so-fancy
+ln -sfn ~/src/diff-so-fancy/diff-so-fancy ~/.local/bin/
 EOF
