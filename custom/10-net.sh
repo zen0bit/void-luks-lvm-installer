e38ea79afb54efc93eaf49530b6fb4b62fff2c0c
Ivan Mirić <ivan@imiric.com>
2019-05-08 19:24:42 +0200

    fix(custom): enable wpa_supplicant as dhcpcd hook

--- custom/10-net.sh
+++ custom/10-net.sh
@@ -3,8 +3,8 @@
 xbps-install -Sy curl dhcpcd openbsd-netcat wifi-firmware wget wpa_supplicant || true
 
 # Enable services
+ln -sfn /usr/share/dhcpcd/hooks/10-wpa_supplicant /usr/libexec/dhcpcd-hooks
 ln -sfn /etc/sv/dhcpcd /etc/runit/runsvdir/default/
-ln -sfn /etc/sv/wpa_supplicant /etc/runit/runsvdir/default/
 
 # Download and compile Surf
 xbps-install -Sy gcr-devel glib-devel rofi webkit2gtk-devel || true
