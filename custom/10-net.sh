1a1c80ac9bbf20863fa86b779b6906b243642b52
Ivan Mirić <ivan@imiric.com>
2019-05-08 19:25:42 +0200

    fix(custom): clone surf/st as root, fix perms after
    This is because the GPG SSH socket is only readable by root.

--- custom/10-net.sh
+++ custom/10-net.sh
@@ -8,13 +8,15 @@ ln -sfn /etc/sv/dhcpcd /etc/runit/runsvdir/default/
 
 # Download and compile Surf
 xbps-install -Sy gcr-devel glib-devel rofi webkit2gtk-devel || true
-su - ${USERACCT} <<'EOF'
 export GIT_SSH_COMMAND='ssh -o Hostname=172.16.1.5 -o Port=10022' \
-  SSH_AUTH_SOCK=/tmp/.gnupg/S.gpg-agent.ssh
-test -d ~/Projects/surf || git clone git@git.imiric.lan:ivan/surf.git ~/Projects/surf
-(cd ~/Projects/surf && make install)
+  SSH_AUTH_SOCK=/tmp/.gnupg/S.gpg-agent.ssh \
+  PROJECTS_DIR=/home/${USERACCT}/Projects
+mkdir -p "$PROJECTS_DIR"
+test -d "$PROJECTS_DIR/surf" || git clone git@git.imiric.lan:ivan/surf.git "$PROJECTS_DIR/surf"
+(cd "$PROJECTS_DIR/surf" && make install)
 
 # st is needed for surf's download functionality
-test -d ~/Projects/st || git clone git@git.imiric.lan:ivan/st.git ~/Projects/st
-(cd ~/Projects/st && make install)
-EOF
+test -d "$PROJECTS_DIR/st" || git clone git@git.imiric.lan:ivan/st.git "$PROJECTS_DIR/st"
+(cd "$PROJECTS_DIR/st" && make install)
+
+chown -R ${USERACCT}: /home/${USERACCT}/Projects
