2980c4185cc31b8821a30b8f711dcbc9993150dc
Ivan Mirić <ivan@imiric.com>
2019-05-08 19:41:17 +0200

    fix(custom): ensure dir exists

--- custom/10-programming.sh
+++ custom/10-programming.sh
@@ -9,6 +9,6 @@ if [ -n "$USERACCT" ]; then
   su - ${USERACCT} <<'EOF'
 mkdir -p ~/src
 test -d ~/src/diff-so-fancy || git clone https://github.com/so-fancy/diff-so-fancy.git ~/src/diff-so-fancy
-ln -sfn ~/src/diff-so-fancy/diff-so-fancy ~/.local/bin/
+mkdir -p ~/.local/bin && ln -sfn ~/src/diff-so-fancy/diff-so-fancy ~/.local/bin/
 EOF
 fi
