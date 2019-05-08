4c821de71383cb81afd0713622c83888b525983c
Ivan Mirić <ivan@imiric.com>
2019-05-08 19:46:05 +0200

    fix(custom): pyenv paths

--- custom/10-python.sh
+++ custom/10-python.sh
@@ -6,15 +6,15 @@ xbps-install -Sy bzip2-devel libffi-devel liblzma-devel libressl-devel \
 
 # Install pyenv, the specified Python version and additional packages
 su -w PYTHON_VERSION - ${USERACCT} bash -c '
-PYENV_ROOT=$HOME/.pyenv PATH=$PYENV_ROOT/bin:$PATH
-eval "$(pyenv init -)"
+export PYENV_ROOT=$HOME/.pyenv PATH=$HOME/.pyenv/bin:$PATH
 test -d $PYENV_ROOT || git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
-pyenv install --skip-existing ${PYTHON_VERSION}
+eval "$(pyenv init -)"
+MAKE_OPTS="-j " pyenv install --skip-existing ${PYTHON_VERSION}
 test -d $HOME/.pyenv/plugins/pyenv-virtualenv || git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
-pyenv global ${PYHON_VERSION}
+pyenv global ${PYTHON_VERSION}
 
 # Python packages
-pip install glances pinboard s-tui
+$(pyenv which pip) install glances pinboard s-tui
 
 # Fix bug in pinboard package
 chmod +x $(pyenv which pinboard)
