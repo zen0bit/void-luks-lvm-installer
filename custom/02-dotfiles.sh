069802ea5e53da99b2be8e97fff3b0d77fadc43a
Ivan Mirić <ivan@imiric.com>
2019-05-08 21:48:03 +0200

    fix(custom): correct known_hosts directory

--- custom/02-dotfiles.sh
+++ custom/02-dotfiles.sh
@@ -2,7 +2,7 @@
 
 # Copy the known_hosts for host verification
 mkdir -m 700 -p /home/ivan/.ssh
-cp $(dirname "$0")/known_hosts /home/ivan/.ssh/
+cp $(dirname "$0")/files/known_hosts /home/ivan/.ssh/
 
 test -d /home/ivan/.files || \
 GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=/home/ivan/.ssh/known_hosts -o Hostname=172.16.1.5 -o Port=10022' \
