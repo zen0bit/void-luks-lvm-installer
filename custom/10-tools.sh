625418156bdaf2babf58897d68b7d6d4c3532d2f
Ivan Mirić <ivan@imiric.com>
2019-11-04 20:34:06 +0100

    feat(custom): add a few more custom tools

--- custom/10-tools.sh
+++ custom/10-tools.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-xbps-install -Sy dtrx pandoc par2cmdline parted pass pixz p7zip unrar words-mnemonic zip || true
+xbps-install -Sy dtrx lf pandoc par2cmdline parted pass pass-otp pixz p7zip \
+  translate-shell unrar words-mnemonic zip || true
