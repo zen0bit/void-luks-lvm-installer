1455a46995e00d5d5b96611924deca1ebdaa3bc1
Ivan Mirić <ivan@imiric.com>
2019-11-23 15:16:04 +0100

    feat: move scripts to bin/

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@ set -euo pipefail
 
 # Install a Void Linux system on a LUKS encrypted volume mounted at /mnt.
 
-./mkpart.sh
-./mount.sh
-./install-base.sh
-./install-custom.sh
-./reconfigure.sh
+PATH="$PWD/bin:$PATH"
+
+mkpart.sh
+mount.sh
+install-base.sh
+install-custom.sh
+reconfigure.sh
 
 sync
